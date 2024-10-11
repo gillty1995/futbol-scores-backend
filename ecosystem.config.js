01f6afc497ad3dc3514ddb991983f1857c5f2709
gillty <gooee@mac.com>
2024-10-11 16:03:59 -0500

    fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
       name: "app",
       script: "./app.js",
       env: {
-        JWT_SECRET:
-          "0ee11c60e2b084a9abf7a067eea03e7a12023cb0bd7cdf165165c6f1ef5a6387",
-        MONGO_URL: "mongodb://Gill:Sniffer95@127.0.0.1:27017/futbol_scores",
+        JWT_SECRET: process.env.JWT_SECRET,
+        MONGO_URL: process.env.MONGO_URL,
         NODE_ENV: "production",
       },
     },
