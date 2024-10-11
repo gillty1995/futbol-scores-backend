1b5eacc191a9431cabd5685ce221b23d6be6efad
gillty <gooee@mac.com>
2024-10-11 13:34:08 -0500

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "./app.js",
+      env: {
+        JWT_SECRET:
+          "0ee11c60e2b084a9abf7a067eea03e7a12023cb0bd7cdf165165c6f1ef5a6387",
+        MONGO_URL: "mongodb://127.0.0.1:27017/futbol_scores",
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
